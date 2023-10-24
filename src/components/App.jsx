aa0586a15a469ba3414c8eb3e3e2de7c0385986b
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2023-10-24 07:59:55 +0200

    all done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,18 +6,20 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { Container, MainText, SecondMainText } from './App.styled';
 
+const initialValue = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export const App = () => {
   const [contacts, setContacts] = useState(() => {
     const savedContacts = localStorage.getItem('contacts');
     if (savedContacts !== null) {
       return JSON.parse(savedContacts);
     }
-    return [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ];
+    return initialValue;
   });
 
   const [filter, setFilter] = useState('');
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -13,7 +13,7 @@ const nameSchema = Yup.object().shape({
     .min(2, 'Too Short!')
     .max(25, 'Too Long!')
     .required('Required'),
-  number: Yup.number().required('Required').integer(),
+  // number: Yup.number().required('Required').integer(), - No valid
 });
 
 export const ContactForm = props => {
@@ -33,12 +33,18 @@ export const ContactForm = props => {
         <Form>
           <EntryField>
             <FormLabel htmlFor="name">First Name</FormLabel>
-            <Field id="name" name="name" placeholder="Enter Name" />
+            <Field id="name" name="name" placeholder="Name" />
           </EntryField>
 
           <EntryField>
             <FormLabel htmlFor="number">Telephone</FormLabel>
-            <Field id="number" name="number" placeholder="Enter Telephone" />
+            <Field
+              id="number"
+              type="tel"
+              pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
+              name="number"
+              placeholder="xxx-xx-xx"
+            />
           </EntryField>
 
           <Button type="submit">Submit</Button>
